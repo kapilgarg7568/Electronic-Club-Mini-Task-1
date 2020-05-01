81cc5d0141312ef06b1adaeaddd6e1aff3a0fbf7
kapilgarg7568 <64272528+kapilgarg7568@users.noreply.github.com>
2020-05-01 16:30:26 +0530

    Update and rename project-21.m to Raspberry Pi GPS tracker.m

---
+++ Raspberry Pi GPS tracker.m
@@ -0,0 +1,11 @@
+# **Raspberry Pi GPS tracker**
+its a simple Raspberry Pi GPS tracker and real time web interface based on Raspberry Pi which tracks the position.
+we will use a gps module here.
+we have a GPS module with a serial communication that we will read on Raspberry and show it on a HTML web page.
+For reading from the module we have to write a simple Python code to get longitudes and latitudes
+
+###### ***GPS Module***
+GPS receivers use a constellation of satellites and ground stations to compute position and time almost anywhere on earth. With this information and some math, a ground based receiver or GPS module can calculate its position and time.
+
+###### ***HTML (HyperText Markup Language)***
+It is the most basic building block of the Web. It defines the meaning and structure of web content. Hypertext" refers to links that connect web pages to one another, either within a single website or between websites. Links are a fundamental aspect of the Web. By uploading content to the Internet and linking it to pages created by other people, you become an active participant in the World Wide Web.
--- project-21.m
+++
@@ -1,6 +0,0 @@
-Raspberry Pi GPS tracker:-->
-its a simple Raspberry Pi GPS tracker and real time web interface based on Raspberry Pi which tracks the position.
-we will use a gps module here.
-GPS Module:GPS receivers use a constellation of satellites and ground stations to compute position and time almost anywhere on earth. ... With this information and some math, a ground based receiver or GPS module can calculate its position and time.
-we have a GPS module with a serial communication that we will read on raspberry and show it on a HTML web page.
-For reading from the module we have to write a simple python code to get longitudes and latitudes
